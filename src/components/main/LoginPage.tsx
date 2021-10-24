e0371e7dd2d8d40765ade03b2df9547a708c6d46
Daniel Chong <temporaryaion1@hotmail.com>
2021-10-24 23:16:40 +0900

    style LoginPage and add email/password validation functions

--- src/components/main/LoginPage.tsx
+++ src/components/main/LoginPage.tsx
@@ -2,27 +2,35 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
+import { Button } from "@mui/material";
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <Box width="100%" margin="0 auto">
-      <Typography variant="h3">Login page</Typography>
-      <TextField
-        value={email}
-        id="email"
-        label="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <TextField
-        value={password}
-        id="password"
-        label="Password"
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+    <Box width="100%" margin="0 auto" textAlign="center">
+      <Typography variant="h4" marginTop="40px">
+        Login
+      </Typography>
+      <Box display="flex" flexDirection="column" width="30%" margin="0 auto">
+        <TextField
+          value={email}
+          id="email"
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          margin="normal"
+        />
+        <TextField
+          value={password}
+          id="password"
+          label="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          margin="normal"
+        />
+        <Button>Log in</Button>
+      </Box>
     </Box>
   );
 };
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,3 +1,28 @@
-const usernameConstraints = {
-  regex: /a/,
+const emailConstraints = {
+  // regex from: https://regexr.com/3e48o
+  regex: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+  minLength: 5,
+  maxLength: 50,
+};
+
+const passwordContraints = {
+  regex: /^[a-zA-Z0-9_\-!@#$%^&*()]{4,16}$/,
+  minLength: 4,
+  maxLength: 16,
+};
+
+export const validateEmail = (email: string): boolean => {
+  const { regex, minLength, maxLength } = emailConstraints;
+
+  if (email.length < minLength || email.length > maxLength) return false;
+
+  return email.match(regex) !== null;
+};
+
+export const validatePassword = (password: string): boolean => {
+  const { regex, minLength, maxLength } = passwordContraints;
+
+  if (password.length < minLength || password.length > maxLength) return false;
+
+  return password.match(regex) !== null;
 };
